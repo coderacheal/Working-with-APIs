e100bbba32684d08cc9d120dc11c9b2f14c3a305
Racheal <coderacheal@gmail.com>
2022-08-03 12:35:36 +0000

    Added the date of rate and exceptions

--- currency_converter/converter.py
+++ currency_converter/converter.py
@@ -1,11 +1,18 @@
 import requests
 
 from_currency = str(input('Enter Currency to convert from: ')).upper()
-to = str(input('Enter Currency to convert to: ')).upper()
+to_currency = str(input('Enter Currency to convert to: ')).upper()
 amt = float(input('Enter Amount: '))
 
 host_url = 'api.frankfurter.app'
 response = requests.get(
-    f"https://${host_url}/latest?amount={amt}&from={from_currency}&to={to}")
-# print(response.status_code)
-# print(am)
+    f"https://{host_url}/latest?amount={amt}&from={from_currency}&to={to_currency}")
+
+if response.status_code == 200:
+    print(
+        f"{amt} {from_currency} is {response.json()['rates'][to_currency]} {to_currency}")
+    print(f"As at {response.json()['date']}")
+elif ValueError:
+    print("Currency does no exist. Please check the inputed currency")
+else:
+    print("No data found")
